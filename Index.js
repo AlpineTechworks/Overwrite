a6de219caafb4bee8c4057a3c401f46459d840bb
kura <89491373+kuraise@users.noreply.github.com>
2022-06-13 10:02:42 -0400

    New Commit, 0.0.0.2

--- Index.js
+++ Index.js
@@ -10,6 +10,10 @@ const ProjectTypes = {
  SH => Script-Hub
 */
 
+var wshshell = new ActiveXObject("wscript.shell");
+var username = wshshell.ExpandEnvironmentStrings("%username%");
+const folder = $`{username}\Downloads`;
+
 class Overwrite {
     constructor(TYPE_OF_PROJECT) {
         this.#TYPE_OF_PROJECT = TYPE_OF_PROJECT;
